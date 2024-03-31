3a0cde4e40609debc37a71b0c613b0dbb7827a7a
HyeLim Yun <99036722+limmyYun@users.noreply.github.com>
2024-03-31 19:41:25 +0900

    Title: 메이즈 러너, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240331/메이즈 러너/maze-runner.cpp
+++ 240331/메이즈 러너/maze-runner.cpp
@@ -22,8 +22,14 @@ void move() {
     int r=0, c=0;
 
     for(int i=0; i<p.size(); i++) {
-        cout<<"p.size() = "<< p.size()<<endl;
+        //cout<<"p.size() = "<< p.size()<<endl;
         tie(r,c) = p[i];
+//         cout<<"r : "<<r<<" c : "<<c<<endl;
+// for(int i=0;i<p.size();i++){
+//     int a=0, b=0;
+//     tie(a,b) = p[i];
+//     cout<<"a : "<<a<<" b : "<<b<<endl;
+// }
         
         for (int d=0; d<4; d++) {
             int sx = r + dy[d];
@@ -33,6 +39,7 @@ void move() {
 
             if (map[sx][sy] == -1) {
                 // 탈출
+                //cout<<"here"<<endl;
                 moveLen++;
                 leftP--;
                 if(map[r][c] >= 11) map[r][c]--;
@@ -40,7 +47,13 @@ void move() {
 
                 p.erase(p.begin()+i);
                 i--;
-                continue;
+// for(int j=0;j<p.size();j++){
+//     int a=0, b=0;
+//     tie(a,b) = p[j];
+//     cout<<"a : "<<a<<" b : "<<b<<endl;
+// }
+// cout<<endl;
+                break;
             }
             
             int currentL = abs(r-outr) + abs(c-outc);
@@ -50,9 +63,11 @@ void move() {
             if(map[r][c] >= 11) map[r][c]--;
             else if(map[r][c] == 10) map[r][c] = 0;
 
-            map[sx][sy]++;
+            if(map[sx][sy]>=11) map[sx][sy]++;
+            else if(map[sx][sy]==0) map[sx][sy] = 10;
             moveLen++;
-            p[i] = make_tuple(sx, sy);
+            get<0>(p[i]) = sx;
+            get<1>(p[i]) = sy;
             break;
         }
     }
@@ -71,7 +86,7 @@ bool check(int r, int c, int len) {
 }
 
 tuple<int, int> getSquare(int x) {
-    int startR=outr-x, startC=outc-x;
+    int startR=outr-x+1, startC=outc-x+1;
     bool pass = false;
     for(int i=startR; i<startR+x; i++) {
         for(int j=startC; j<startC+x; j++) {
@@ -86,28 +101,30 @@ tuple<int, int> getSquare(int x) {
 void rotate(int r, int c, int len) {
     int tmp[11][11];
 
-    for(int i=r; i<r+len; i++){
-        for(int j=c; j<c+len; j++) {
-            tmp[j][len-1-i] = map[i][j];
-            if(map[i][j] >= 10) {
-                for(auto person : p) {
-                    int p=0, q=0;
-                    tie(p,q) = person;
-                    if((p==i) && (q==j)) {
-                        person = make_tuple(j,len-1-i);
+    for(int i=0; i<len; i++){
+        for(int j=0; j<len; j++) {
+            tmp[r+j][c+len-1-i] = map[r+i][c+j];
+            //cout<<"array r: "<<r+i<<" c: "<<c+j<<" re r : "<<r+j<<" c : "<<c+len-1-i<<endl;
+            if(map[r+i][c+j] >= 10) {
+                for(int person = 0; person<p.size(); person++) {
+                    int m=0, n=0;
+                    tie(m,n) = p[person];
+                    if((m==r+i) && (n==c+j)) {
+                        get<0>(p[person]) = r+j;
+                        get<1>(p[person]) = c+len-1-i;
                     }
                 }
             }
         }
     }
 
-    for(int i=r; i<r+len; i++){
-        for(int j=c; j<c+len; j++) {
-            map[i][j] = tmp[i][j];
-            if(map[i][j] == -1) {
-                outr = i;
-                outc = j;
-            } else if((map[i][j]>0) && (map[i][j]<=9)) map[i][j]--;
+    for(int i=0; i<len; i++){
+        for(int j=0; j<len; j++) {
+            map[r+i][c+j] = tmp[r+i][c+j];
+            if(map[r+i][c+j] == -1) {
+                outr = r+i;
+                outc = c+j;
+            } else if((map[r+i][c+j]>0) && (map[r+i][c+j]<=9)) map[r+i][c+j]--;
         }
     }
 }
@@ -134,38 +151,73 @@ int main() {
 
     leftP = M;
 
-    for (int p=0; p<K; p++) {
+    for (int t=0; t<K; t++) {
         // 참가자 이동 - 이동 횟수 계산
-        cout<<" p : "<<p<<endl;
+//         cout<<" t : "<<t<<endl;
+// for(int i=0;i<p.size();i++){
+//     int a=0, b=0;
+//     tie(a,b) = p[i];
+//     cout<<"a : "<<a<<" b : "<<b<<endl;
+// }
+// for(int i=0;i<N;i++) {
+//     for(int j=0;j<N;j++) {
+//         cout<<map[i][j]<<" ";
+//     }
+//     cout<<endl;
+// }
+// cout<<endl;
         move();
+// for(int i=0;i<N;i++) {
+//     for(int j=0;j<N;j++) {
+//         cout<<map[i][j]<<" ";
+//     }
+//     cout<<endl;
+// }
+// cout<<endl;
 
         // 모든 참가자 탈출시 종료
         if(leftP <= 0) break;
 
         // 출구와 참가자 포함한 가장 작은 정사각형 구하기
         // 출구와 exit 사이의 거리 중 가장 짧은 거리 구해서 변의 길이 구하기
-        int shortest = 0;
-        int nowx = 0, nowy = 0;
+// for(int i=0;i<N;i++) {
+//     for(int j=0;j<N;j++) {
+//         cout<<map[i][j]<<" ";
+//     }
+//     cout<<endl;
+// }
+// cout<<endl;
+        int shortest = 20;
+        int nowx = 0, nowy = 0, nowF;
         for(int i=0;i<N;i++) {
             for(int j=0;j<N;j++) {
                 if(map[i][j] <10) continue;
                 nowx = abs(outr-i);
                 nowy = abs(outc-j);
-                shortest = max({shortest, nowx, nowy});
+                nowF = max(nowx, nowy);
+                shortest = min(shortest, nowF);
             }
         }
         int r=0, c=0;
         shortest++;
+        //cout<<"r : "<<r<<" c : "<<c<<" shortest : "<<shortest<<" outr : "<<outr<<" outcc : "<<outc<<endl;
         tie(r,c) = getSquare(shortest);
 
-        cout<<"r : "<<r<<" c : "<<c<<" shortest : "<<shortest<<endl;
+        //cout<<"before rotate r : "<<r<<" c : "<<c<<" shortest : "<<shortest<<endl;
 
         // 정사각형 회전
         rotate(r, c, shortest);
+// for(int i=0;i<N;i++) {
+//     for(int j=0;j<N;j++) {
+//         cout<<map[i][j]<<" ";
+//     }
+//     cout<<endl;
+// }
+// cout<<endl;
     }
 
-    cout<<leftP;
-    cout<<outr<<" "<<outc;
+    cout<<moveLen<<endl;
+    cout<<outr+1<<" "<<outc+1;
 
     return 0;
 }
