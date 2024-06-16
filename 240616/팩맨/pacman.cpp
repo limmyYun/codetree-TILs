7bfac4f7b2ea172fc3e978b3baa490fa34ecc8e5
HyeLim Yun <99036722+limmyYun@users.noreply.github.com>
2024-06-16 23:37:05 +0900

    Title: 팩맨, Time: 14ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240616/팩맨/pacman.cpp
+++ 240616/팩맨/pacman.cpp
@@ -6,10 +6,10 @@
 
 using namespace std;
 
-int mdx[8] = { 0, -1, -1, -1, 0, 1, 1, 1};
-int mdy[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
-int pdx[4] = { 0, -1, 0, 1};
-int pdy[4] = {-1, 0, 1, 0};
+int mdx[8] = { 0, -1, -1, -1, 0, 1, 1, 1 };
+int mdy[8] = { -1, -1, 0, 1, 1, 1, 0, -1 };
+int pdx[4] = { 0, -1, 0, 1 };
+int pdy[4] = { -1, 0, 1, 0 };
 
 
 // 마리 수, 턴 수
@@ -23,69 +23,69 @@ int died[5][5];
 // 몬스터 알 배열
 queue<int> babyMonster[5][5];
 
-void printMonsterSize(){
-    cout<<"printMonsterSize"<<endl;
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
-            cout<<monster[i][j].size()<<" ";
+void printMonsterSize() {
+    cout << "printMonsterSize" << endl;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << monster[i][j].size() << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
-    cout<<endl;
+    cout << endl;
 }
 
-void printdied(){
-    cout<<"printdied"<<endl;
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
-            cout<<died[i][j]<<" ";
+void printdied() {
+    cout << "printdied" << endl;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << died[i][j] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
-    cout<<endl;
+    cout << endl;
 }
 
-void makeBabyMoster(){
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
+void makeBabyMoster() {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
             babyMonster[i][j] = monster[i][j];
         }
     }
 }
 
-void moveMonsters(){
+void moveMonsters() {
     queue<int> newMonster[5][5];
     int queSize = 0;
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
             queSize = monster[i][j].size();
 
-            for(int q = 0; q<queSize; q++) {
+            for (int q = 0; q < queSize; q++) {
                 bool isOut = false;
                 int monsterDir = monster[i][j].front();
                 monster[i][j].pop();
 
-                for(int d0=monsterDir; d0<monsterDir+8; d0++){
-                    if(isOut) break;
+                for (int d0 = monsterDir; d0 < monsterDir + 8; d0++) {
+                    if (isOut) break;
 
-                    int d = d0%8;
+                    int d = d0 % 8;
                     int sr = i + mdy[d];
                     int sc = j + mdx[d];
-                    if(sr<0 || sc<0 || sr>=4 || sc>=4) continue;
-                    if((sr==pr)&&(sc==pc)) continue;
-                    if(died[sr][sc] != 0) continue;
+                    if (sr < 0 || sc < 0 || sr >= 4 || sc >= 4) continue;
+                    if ((sr == pr) && (sc == pc)) continue;
+                    if (died[sr][sc] != 0) continue;
 
                     newMonster[sr][sc].push(d);
                     isOut = true;
                 }
-                if(!isOut){
+                if (!isOut) {
                     newMonster[i][j].push(monsterDir);
                 }
             }
         }
     }
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
             monster[i][j] = newMonster[i][j];
         }
     }
@@ -104,13 +104,14 @@ int tmpKill(int fm, int sm, int tm) {
     memset(visited, false, sizeof(visited));
     //visited[gr][gc] = true;
 
-    for(int dir = 0; dir<d.size(); dir++) {
+    for (int dir = 0; dir < d.size(); dir++) {
         gr = gr + pdy[d[dir]];
         gc = gc + pdx[d[dir]];
-        if(gr<0 || gc<0 || gr>=4 || gc>=4 || visited[gr][gc]) {
+        if (gr < 0 || gc < 0 || gr >= 4 || gc >= 4) {
             num = 0;
             break;
         }
+        if(visited[gr][gc]) continue;
         visited[gr][gc] = true;
         num += monster[gr][gc].size();
     }
@@ -122,13 +123,13 @@ tuple<int, int, int> findBestWay() {
 
     int numDied = 0;
     int bestDied = 0;
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
-            for(int k=0; k<4; k++){
-                numDied = tmpKill(i,j,k);
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            for (int k = 0; k < 4; k++) {
+                numDied = tmpKill(i, j, k);
 
-                if(numDied>bestDied) {
-                    best = make_tuple(i,j,k);
+                if (numDied > bestDied) {
+                    best = make_tuple(i, j, k);
                     bestDied = numDied;
                 }
             }
@@ -146,17 +147,17 @@ void moveMan() {
     way.push_back(s);
     way.push_back(th);
 
-    //cout<<f<<" "<<s<<" "<<th<<endl;
+    //cout << f << " " << s << " " << th << endl;
 
-    for(int i=0;i<3;i++){
+    for (int i = 0; i < 3; i++) {
         int newPr = pr + pdy[way[i]];
         int newPc = pc + pdx[way[i]];
 
         // 몬스터 que에서 빼기
         // 몬스터 시체에 추가하기
         int size = monster[newPr][newPc].size();
-        if(size>0) {
-            while(!monster[newPr][newPc].empty()) monster[newPr][newPc].pop();
+        if (size > 0) {
+            while (!monster[newPr][newPc].empty()) monster[newPr][newPc].pop();
             died[newPr][newPc] = 3;
         }
         pr = newPr;
@@ -165,20 +166,20 @@ void moveMan() {
 }
 
 void clean() {
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
-            if(died[i][j] != 0) died[i][j]--;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (died[i][j] != 0) died[i][j]--;
         }
     }
 }
 
 void newPush() {
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
-            if(babyMonster[i][j].size() == 0) continue;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (babyMonster[i][j].size() == 0) continue;
 
             int size = babyMonster[i][j].size();
-            for(int b = 0; b<size; b++) {
+            for (int b = 0; b < size; b++) {
                 int dir = babyMonster[i][j].front();
                 babyMonster[i][j].pop();
 
@@ -189,26 +190,26 @@ void newPush() {
 }
 
 int main() {
-    cin>>m>>t;
-    cin>>pr>>pc;
+    cin >> m >> t;
+    cin >> pr >> pc;
     pr--, pc--;
 
     int mr = 0, mc = 0, md = 0;
-    for(int i = 0;i<m;i++){
-        cin>>mr>>mc>>md;
+    for (int i = 0; i < m; i++) {
+        cin >> mr >> mc >> md;
         mr--, mc--, md--;
         monster[mr][mc].push(md);
     }
 
     //printMonsterSize();
 
-    for(int ct = 0; ct<t; ct++){
-        //cout<<"turn : "<<ct+1<<endl;
+    for (int ct = 0; ct < t; ct++) {
+        //cout << "turn : " << ct + 1 << endl;
         // 1. 몬스터 복제
         makeBabyMoster();
 
         //printMonsterSize();
-        
+
         // 2. 몬스터 이동
         moveMonsters();
 
@@ -222,17 +223,17 @@ int main() {
 
         // 4. 시체 소멸
         clean();
-        
+
         // 5. 몬스터 복제
         newPush();
     }
 
     int result = 0;
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
             result += monster[i][j].size();
         }
     }
-    cout<<result<<endl;
+    cout << result << endl;
     return 0;
 }
