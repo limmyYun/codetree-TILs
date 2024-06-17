cf447ce993832fc2d6059a0b2f884323d6481e50
HyeLim Yun <99036722+limmyYun@users.noreply.github.com>
2024-06-17 21:52:15 +0900

    Title: 격자 위의 편안한 상태, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240617/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {-1, 0, 1, 0};
+
+bool map[101][101];
+
+bool check(int r, int c) {
+    int num = 0;
+    for(int d=0; d<4; d++){
+        int sr = r+dy[d];
+        int sc = c+dx[d];
+        if(sr<0 || sc<0 || sr>=n || sc>=n) continue;
+        if(!map[sr][sc]) continue;
+        num++;
+    }
+
+    if(num>=3) return true;
+    else return false;
+}
+
+int main() {
+    cin>>n>>m;
+
+    int r = 0, c = 0;
+    bool isGood = false;
+    for(int i = 0; i<m; i++) {
+        cin>>r>>c;
+        r--, c--;
+        isGood = check(r,c);
+        map[r][c] = true;
+        cout<<isGood<<endl;
+    }
+    return 0;
+}
